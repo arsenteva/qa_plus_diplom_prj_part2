81f35ac419e7fa8ebb94083faf70675e8d642b87
arsenteva <arsentevaa@gmail.com>
2023-12-18 08:41:07 +0300

    delete DOC_PATH from configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,4 @@
 # Анна Арсентьева, 11 когорта, Инженер по тестированию плюс, финальный проект, часть 2
 URL_SERVICE = "https://30f26ca9-bb6e-451a-a3d6-f66b4fb7c5f6.serverhub.praktikum-services.ru"
-DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_INFO_PATH = "/api/v1/orders/track"
